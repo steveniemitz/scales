35ab68370a3c94fa535d4a69d3e0d93c970d814a
steve <sniemitz@twitter.com>
2015-07-17 18:22:11 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+  name='scales',
+  version='1.0.0',
+  author='Steve Niemitz',
+  author_email='sniemitz@twitter.com',
+  url='https://www.github.com/steveniemitz/scales',
+  packages=['scales',
+            'scales.http',
+            'scales.loadbalancer',
+            'scales.pool',
+            'scales.thrift',
+            'scales.thrifthttp',
+            'scales.thriftmux'],
+  install_requires=[
+      'gevent>=0.13.8',
+      'thrift>=0.5.0',
+      'kazoo>=1.3.1',
+      'requests>=2.0.0']
+)
